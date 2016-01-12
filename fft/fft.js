4aad1b38c6abf5180ad23e7f0b2ccd8841fcc3dc
Flemming Stäbler <stu116021@mail.uni-kiel.de>
2016-01-12 23:52:24 +0100

    worked a bit on the rendering instead of making the fft better.

--- fft/fft.js
+++ fft/fft.js
@@ -19,6 +19,8 @@ var fft = function (data) {
   return resultRe;
 };
 
+var fft2 = fft;
+
 var rFFT = function (dataRe, dataIm) {
   var n = dataRe.length;
   if (n == 1) return [dataRe, dataIm];
@@ -56,39 +58,4 @@ var rFFT = function (dataRe, dataIm) {
   return [dataRe, dataIm];
 };
 
-/*
-function cfft(amplitudes)
-{
-	var N = amplitudes.length;
-	if( N <= 1 )
-		return amplitudes;
-
-	var hN = N / 2;
-	var even = [];
-	var odd = [];
-	even.length = hN;
-	odd.length = hN;
-	for(var i = 0; i < hN; ++i)
-	{
-		even[i] = amplitudes[i*2];
-		odd[i] = amplitudes[i*2+1];
-	}
-	even = cfft(even);
-	odd = cfft(odd);
-
-	var a = -2*Math.PI;
-	for(var k = 0; k < hN; ++k)
-	{
-		if(!(even[k] instanceof Complex))
-			even[k] = new Complex(even[k], 0);
-		if(!(odd[k] instanceof Complex))
-			odd[k] = new Complex(odd[k], 0);
-		var p = k/N;
-		var t = new Complex(0, a * p);
-		t.cexp(t).mul(odd[k], t);
-		amplitudes[k] = even[k].add(t, odd[k]);
-		amplitudes[k + hN] = even[k].sub(t, even[k]);
-	}
-	return amplitudes;
-}*/
 fft = rFFT; //debug
